fe5448bf0f6019430e85c85bdfc0cc92b6d22fb6
salvatore-arienzo <32364699+salvatore-arienzo@users.noreply.github.com>
2020-04-26 15:54:42 +0200

    update framing

--- dataset_framing/dataset_framing.py
+++ dataset_framing/dataset_framing.py
@@ -16,27 +16,28 @@ from IPython.display import display
 
 # Set your input/output paths here
 
-# In[7]:
+# In[3]:
 
 
 inputDataViolence = "dataset/Violence/"
 inputDataNonViolence = "dataset/NonViolence/"
 outputDataViolence = "framed_dataset/Violence/"
-outputDataNonViolence = "framed_dataset/Non/Violence/"
+outputDataNonViolence = "framed_dataset/NonViolence/"
 
 
 # Set your image width and height and the total number of videos
 
-# In[9]:
+# In[4]:
 
 
 imgX = 224   
 imgY = 224
 size = (imgX,imgY)
 totalVideos = 1001 #total numeber of Videos
+nFrame = 0 #currentFrame Expected: 159000 for the first part,280000 for the second part 
 
 
-# In[10]:
+# In[5]:
 
 
 nVideo = 1 #currentVideo
@@ -44,21 +45,20 @@ f = IntProgress(min=0, max=totalVideos) # instantiate the ProgressBar
 display(f) # display the progressBar
 for nVideo in range(totalVideos):
     cap = cv2.VideoCapture(inputDataViolence +"V_"+str(nVideo)+".mp4")
-    i = 0
     while(cap.isOpened()):
         ret,frame = cap.read()
         if ret == False:
             break
         resize = cv2.resize(frame,size)
-        cv2.imwrite(outputDataViolence +"video"+str(nVideo)+"img"+str(i)+".jpg",resize)
-        i+=1
+        cv2.imwrite(outputDataViolence +"img"+str(nFrame)+".jpg",resize)
+        nFrame+=1
     cap.release()
     cv2.destroyAllWindows()
     nVideo+=1
     f.value+=1
 
 
-# In[11]:
+# In[6]:
 
 
 nVideo = 1 #currentVideo
@@ -72,8 +72,8 @@ for nVideo in range(totalVideos):
         if ret == False:
             break
         resize = cv2.resize(frame,size)
-        cv2.imwrite(outputDataNonViolence+"video"+str(nVideo)+"img"+str(i)+".jpg",resize)
-        i+=1
+        cv2.imwrite(outputDataNonViolence+"img"+str(nFrame)+".jpg",resize)
+        nFrame+=1
     cap.release()
     cv2.destroyAllWindows()
     nVideo+=1
