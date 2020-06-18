d461547e34fb5d5b9d69280cae3c4370c8f60b18
salvatore-arienzo <32364699+salvatore-arienzo@users.noreply.github.com>
2020-06-18 20:25:20 +0200

    Add files via upload

--- demo/set_label.py
+++ demo/set_label.py
@@ -8,7 +8,7 @@ def set_label(predictions):
         if pred==0:
             count=count+1
     
-    if ((100 * count ) / len(predictions) >= 20):
+    if ((100 * count ) / len(predictions) >= 40):
         return print ('\033[1m \033[91m \033[5m' +  "Label: Violence" + '\033[0m')
     else: 
         return print ('\033[3m \033[92m \033[5m' +  "Label: Non Violence" + '\033[0m')
